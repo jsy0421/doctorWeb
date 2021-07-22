b6c66fe98fc4903bb56f1f3f5c45cdaabdb11c24
jasmine <1420775889@qq.com>
2021-07-22 14:40:59 +0800

    调用head组件的例子

---
+++ src/views/调用head组件的例子.vue
@@ -0,0 +1,16 @@
+<template>
+	<Head avatarUrl="https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png" doctorName="无语"
+		doctorPosition="教授" :currentPage=1></Head>
+</template>
+
+<script>
+	import Head from '../components/head.vue'
+	export default {
+		components: {
+			Head
+		}
+	}
+</script>
+
+<style lang="scss">
+</style>
